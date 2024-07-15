26e08bf38baccb128e914d92f7d8688bbb534180
François Bourgin <fbourgin@users.noreply.github.com>
2024-07-15 14:47:07 +0200

    Update build_win.sh

--- recipe/build_win.sh
+++ recipe/build_win.sh
@@ -3,4 +3,4 @@
 set -exuo pipefail
 
 # taken from section 7 of https://uwekorn.com/2020/06/14/r-arrow-for-conda-windows.html
-sed -i -e 's/attribute_visible/__declspec(dllexport)/g' ${BUILD_PREFIX}/Lib/R/library/Rcpp/include/RcppCommon.h
+sed -i -e 's/attribute_visible/__declspec(dllexport)/g' ${BUILD_PREFIX}/lib/R/library/Rcpp/include/RcppCommon.h
