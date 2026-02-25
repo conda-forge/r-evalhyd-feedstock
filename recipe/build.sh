7cf721dc9df1a7b7df02f862b7cf7833736b6de5
François Bourgin <fbourgin@users.noreply.github.com>
2026-02-25 13:57:28 +0100

    Modify build.sh for C++20 configuration
    Updated build script for C++20 support with new flags.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,8 @@
 set -ex
 
 # Added for C++20 support
-CXX20 = g++
-CXX20FLAGS = -g -O2 $(LTO)
-CXX20PICFLAGS = -fpic
-CXX20STD = -std=gnu++20
-SHLIB_CXX20LD = $(CXX20) $(CXX20STD)
-SHLIB_CXX20LDFLAGS = -shared
+echo "CXX20=$CXX" >> ~/.R/Makevars
+echo "CXX20FLAGS=-fPIC -O2 -std=c++20" >> ~/.R/Makevars
 
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
