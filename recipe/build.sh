eaeb04b2e80cd918f0225f1885d8361535bd9d59
François Bourgin <fbourgin@users.noreply.github.com>
2026-02-25 14:22:17 +0100

    Update build script for C++20 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,9 @@
 set -ex
 
 # Added for C++20 support
-echo "CXX20=$CXX" >> ~/.R/Makevars
-echo "CXX20FLAGS=-fPIC -O2 -std=c++20" >> ~/.R/Makevars
+MAKECONF="$PREFIX/lib/R/etc/Makeconf"
+echo "CXX20=$CXX" >> "${MAKECONF}"
+echo "CXX20FLAGS=-fPIC -O2 -std=c++20" >> "${MAKECONF}"
 
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
