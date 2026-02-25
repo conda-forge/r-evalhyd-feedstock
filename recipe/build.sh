4bbb9f5538f435f7783bc06e0c178419b75d467a
François Bourgin <fbourgin@users.noreply.github.com>
2026-02-25 14:32:58 +0100

    Remove CXX20FLAGS from Makeconf
    Removed CXX20FLAGS from Makeconf for C++20 support.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,6 @@ set -ex
 # Added for C++20 support
 MAKECONF="$PREFIX/lib/R/etc/Makeconf"
 echo "CXX20=$CXX" >> "${MAKECONF}"
-echo "CXX20FLAGS=-fPIC -O2 -std=c++20" >> "${MAKECONF}"
 
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
