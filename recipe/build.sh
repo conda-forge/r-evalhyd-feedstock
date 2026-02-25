81dcc02d85ea58854f5eb4f7d8dd6f4e5008605d
François Bourgin <fbourgin@users.noreply.github.com>
2026-02-25 13:47:13 +0100

    Add C++20 support to build.sh
    Added variables for C++20 support in build script.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -ex
 
+# Added for C++20 support
+CXX20 = g++
+CXX20FLAGS = -g -O2 $(LTO)
+CXX20PICFLAGS = -fpic
+CXX20STD = -std=gnu++20
+SHLIB_CXX20LD = $(CXX20) $(CXX20STD)
+SHLIB_CXX20LDFLAGS = -shared
+
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
